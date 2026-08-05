71e50985592574c9e1584c5a5f31e422c893aeec
kakairephilex3-prog <kakairephilex3@gmail.com>
2026-08-05 15:22:49 +0300

    Add initial HTML structure for Philexlisation

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Philexlisation</title>
+</head>
+<body>
 
+<h1>Welcome to Philexlisation</h1>
+
+<p><strong>One Platform. Unlimited Opportunities.</strong></p>
+
+<p>This is the official beginning of the Philexlisation platform.</p>
+
+</body>
+</html>
