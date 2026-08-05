3adab838220c25052fc2541885e8ce45421f997d
kakairephilex3-prog <kakairephilex3@gmail.com>
2026-08-05 16:38:46 +0300

    Add login form with email and password fields

--- login.php
+++ login.php
@@ -1,1 +1,20 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar.php'; ?>
 
+<main>
+    <h1>Login</h1>
+
+    <form action="" method="POST">
+        <label>Email</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Password</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+</main>
+
+<?php include 'includes/footer.php'; ?>
